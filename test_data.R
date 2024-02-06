1b4a9dd435a97672ca5090593157ce3128503ccd
Lexi Knight <Lexi.knight@mail.utoronto.ca>
2024-02-06 03:17:28 +0000

    added tests

---
+++ test_data.R
@@ -0,0 +1,42 @@
+#### Preamble ####
+# Purpose: Tests the cleaned data to ensure easy readability and straightforward access. 
+# Author: Lexi Knight
+# Date: 5 January 2024
+# Contact: lexi.knight@mail.utoronto.ca
+# Pre-requisites: -
+# Any other information needed? -
+
+
+#### Workspace setup ####
+# install packages
+install.packages("tidyverse")
+
+# load packages
+library(tidyverse)
+
+
+#### Test data ####
+
+# read in cleaned data
+undergrad_readings <- read_csv("undergrad_pages_read.csv")
+
+# check day is numeric 
+class(undergrad_readings$'Day') == "numeric"
+
+# check Matt is numeric
+class(undergrad_readings$'Matt') == "numeric"
+
+# check Ash is numeric
+class(undergrad_readings$'Ash') == "numeric"
+
+# check Jackie is numeric
+class(undergrad_readings$'Jackie') == "numeric"
+
+# check Rol is numeric
+class(undergrad_readings$'Rol') == "numeric"
+
+# check Mike is numeric
+class(undergrad_readings$'Mike') == "numeric"
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
